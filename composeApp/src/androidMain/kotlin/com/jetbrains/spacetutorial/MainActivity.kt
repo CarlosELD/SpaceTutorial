a3ee565a13c9eae002d3dc03c5b78c5e0fead037
CarlosELD <s20120006@alumnos.itsur.edu.mx>
2024-05-24 13:23:45 -0600

    Esta es la versión final de kotlin multiplatform

--- composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/MainActivity.kt
@@ -9,15 +9,10 @@ import androidx.compose.ui.tooling.preview.Preview
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setContent {
-            App()
-        }
+        setContent {App()}
     }
 }
 
 @Preview
 @Composable
-fun AppAndroidPreview() {
-    App()
-}
+fun AppAndroidPreview() {App()}
