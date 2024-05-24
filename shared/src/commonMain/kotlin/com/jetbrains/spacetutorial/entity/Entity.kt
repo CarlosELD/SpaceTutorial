7301742b582dd358077fc284b56423862300971d
CarlosELD <s20120006@alumnos.itsur.edu.mx>
2024-05-24 13:07:11 -0600

    Se creo el directorio y la entidad

--- shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/entity/Entity.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/entity/Entity.kt
@@ -6,6 +6,7 @@ import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+
 @Serializable
 data class RocketLaunch(
     @SerialName("flight_number")
